e91abc4edf293a2dcdabac7b5b00377a4cb444a1
pojono <pojono@gmail.com>
2023-07-25 20:04:21 +0200

    set all type checking rules

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ class Hello {
 
   constructor(message: string) {
     this.message = message;
-    console.log('Hello!');
+    console.log(this.message);
   }
 }
 
 try {
+  new Hello('Hello, world!');
+
   const dateString = '2022-03-01';
   console.log(monthFromDate(dateString));
 } catch (err) {
