d7ab63a8c911754687d2343b7d59bbb38c2c37ec
pojono <pojono@gmail.com>
2023-07-27 14:07:06 +0200

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,21 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
     // https://typescript-eslint.io/linting/configs
     // Contains all of recommended,
     // recommended-type-checked, and strict,
     // along with additional strict rules that require type information.
-    "plugin:@typescript-eslint/strict-type-checked",
+    // 1 'plugin:@typescript-eslint/recommended',
+    // 2 'plugin:@typescript-eslint/recommended-type-checked',
+    // 3 'plugin:@typescript-eslint/strict',
+    // 4 'plugin:@typescript-eslint/strict-type-checked',
     // Contains all the stylistic,
     // along with additional stylistic rules that require type information.
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
-    "plugin:prettier/recommended"
+    // 1 'plugin:@typescript-eslint/stylistic',
+    // 2 'plugin:@typescript-eslint/stylistic-type-checked'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:prettier/recommended',
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
